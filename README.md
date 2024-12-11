3a4512df18f632cb78f08d4ad114d5a022ccd429
Rantetsu Inori <56647156+otomad@users.noreply.github.com>
2024-12-11 22:14:52 +0800

    docs: update readme

--- README.md
+++ README.md
@@ -1,2 +1,91 @@
 # intl-ordinal
- Internationalization (i18n) to get the ordinal numbers
+
+[![npm](https://img.shields.io/npm/v/intl-ordinal?logo=npm&logoColor=%23CB3837&label=npm&labelColor=white&color=%23CB3837)](https://www.npmjs.org/package/intl-ordinal)
+[![GitHub](https://img.shields.io/npm/v/intl-ordinal?logo=github&label=GitHub&color=%23181717)](https://github.com/otomad/intl-ordinal)
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)][license-url]
+
+[license-url]: https://opensource.org/licenses/MIT
+[new-issue-url]: https://github.com/otomad/intl-ordinal/issues/new
+
+Internationalization (i18n) to get the ordinal numbers in every languages.
+
+### Example
+
+> #### English:
+> 1st, 2nd, 3rd, 4th …
+> #### Spanish (male):
+> 1.º, 2.º, 3.º …
+> #### Spanish (female):
+> 1.ª, 2.ª, 3.ª …
+
+### Last index and nth
+
+> #### English:
+> last, 2nd to last, 3rd to last …\
+> nth, nth to last
+
+## Installation
+
+```bash
+# npm
+npm install intl-ordinal
+
+# yarn
+yarn add intl-ordinal
+
+# pnpm
+pnpm add intl-ordinal
+```
+
+## Usage
+
+```javascript
+import ordinal from "intl-ordinal";
+
+// Get ordinal numbers
+console.log(ordinal("en-US").format(3)); // "3rd"
+// BigInt
+console.log(ordinal("en-US").format(12n)); // "12th"
+// With gender (defaults to male)
+console.log(ordinal("es-ES").format(5)); // "5.º"
+console.log(ordinal("es-ES", { gender = "female" }).format(5)); // "5.ª"
+// From last
+console.log(ordinal("en-US").format(-1)); // "last"
+console.log(ordinal("en-US").format(-128)); // "128th to last"
+// nth
+console.log(ordinal("en-US").format(Infinity)); // "nth"
+console.log(ordinal("en-US").format(-Infinity)); // "nth to last"
+console.log(ordinal("de-DE").format(Infinity)); // "x-te"
+// Check if the locale is supported\
+console.log(ordinal("en-GB").supports); // true
+console.log(ordinal("art-x-myownlanguage").supports); // false
+```
+
+## License
+
+intl-ordinal is available under the [MIT License][license-url]. See the LICENSE file for more info.
+
+## Supported Languages
+
+If you would like support for other languages, [please submit an issue][new-issue-url].
+
+The resources in the existing languages ​​are obtained through data query, and are not guaranteed to be all correct. [Corrections are welcome][new-issue-url].
+
+<details>
+<summary>Supported languages</summary>
+
+- English
+- Chinese
+- Japanese
+- Korean
+- Vietnamese
+- Indonesian
+- Malay
+- French
+- Spanish
+- Portuguese
+- Italian
+- German
+- Russian
+
+</details>
--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-interface OrdinalFormat {
+export interface OrdinalFormat {
 	/** Does the specified locale has been supported in ordinal format? */
 	readonly supports: boolean;
 
@@ -37,6 +37,8 @@ function f(formatHandler?: (abs: bigint | number, positive: boolean) => string):
 		} satisfies OrdinalFormat);
 }
 
+const isIntPart = (abs: number | bigint, int: bigint) => abs === int || parseInt(String(abs)) === Number(int);
+
 function n(strings: TemplateStringsArray, ...interpolations: (string | number | bigint)[]) {
 	const stringBuilder: string[] = [];
 	let i = 0;
@@ -49,15 +51,28 @@ function n(strings: TemplateStringsArray, ...interpolations: (string | number | 
 	return stringBuilder.join("");
 }
 
+/**
+ * Get the ordinal number formatter.
+ * @param locale - A locale string or a `Intl.Locale` object.
+ * @returns An ordinal number formatter for the specified locale.
+ */
 export default function ordinal(locale: string | Intl.Locale, { gender = "male" }: {
+	/** Gender information for some languages: male or female. Defaults to male. */
 	gender?: "male" | "female";
 } = {}): OrdinalFormat {
 	if (typeof locale === "string") locale = new Intl.Locale(locale);
 	locale = locale.maximize();
 	const { language, script, region } = locale;
+	const isFemale = gender === "female";
 	// const pluralRules = new Intl.PluralRules(locale, { type: "ordinal" });
 	// Intl.PluralRules doesn't support BigInt.
 	switch (language) {
+		/**
+		 * ### English
+		 * 1st, 2nd, 3rd, 4th, …, 11th, 12th, 13th, …, 21st, 22nd, 23rd, …
+		 * last, 2nd to last, 3rd to last, …
+		 * 0th, nth
+		 */
 		case "en":
 			return f((abs, positive) => {
 				const suffix = typeof abs === "number" ? "th" :
@@ -67,44 +82,155 @@ export default function ordinal(locale: string | Intl.Locale, { gender = "male" 
 				const result = n`${abs}${suffix}`;
 				return positive ? result : abs === 1n ? "last" : `${result} to last`;
 			});
+		/**
+		 * ### Chinese
+		 * Simplified:
+		 * 第1、第2、第3、⋯⋯
+		 * 倒数第1、倒数第2、倒数第3、⋯⋯
+		 * 第0、第n
+		 *
+		 * Traditional:
+		 * 第1、第2、第3、⋯⋯
+		 * 倒數第1、倒數第2、倒數第3、⋯⋯
+		 * 第0、第n
+		 */
 		case "zh":
 			return f((abs, positive) => {
 				const result = n`第${abs}`;
 				const prefix = script === "Hant" ? "倒數" : "倒数";
 				return positive ? result : prefix + result;
 			});
+		/**
+		 * ### Japanese
+		 * 1番目、2番目、3番目、⋯⋯
+		 * 最後、最後から2番目、最後から3番目、⋯⋯
+		 * 0番目、n番目
+		 */
 		case "ja":
 			return f((abs, positive) => {
 				const result = n`${abs}番目`;
-				return positive ? result : `最後から${result}`;
+				return positive ? result : abs === 1n ? "最後" : `最後から${result}`;
 			});
+		/**
+		 * ### Korean
+		 * 1번째, 2번째, 3번째, …
+		 * 마지막에서 1번째, 마지막에서 2번째, 마지막에서 3번째, …
+		 * 0번째, n번째
+		 */
 		case "ko":
 			return f((abs, positive) => {
 				const result = n`${abs}번째`;
-				return positive ? result : `${result}에서`;
+				return positive ? result : `마지막에서 ${result}`;
 			});
+		/**
+		 * ### Vietnamese
+		 * thứ 1, thứ 2, thứ 3, …
+		 * thứ 1 đến cuối cùng, thứ 2 đến cuối cùng, thứ 3 đến cuối cùng, …
+		 * thứ 0, thứ n
+		 */
 		case "vi":
 			return f((abs, positive) => {
 				// Unicode mistakenly marked the Vietnamese 1st as a special format.
 				const result = n`thứ ${abs}`;
 				return positive ? result : `${result} đến cuối cùng`;
 			});
+		/**
+		 * ### Indonesian, Malay
+		 * pertama, ke-2, ke-3, …
+		 * terakhir, ke-2 terakhir, ke-3 terakhir, …
+		 * ke-0, ke-n
+		 */
 		case "id":
+		case "ms":
 			return f((abs, positive) => {
+				if (abs === 1n) return positive ? "pertama" : "terakhir";
 				const result = n`ke-${abs}`;
 				return positive ? result : `${result} terakhir`;
 			});
+		/**
+		 * ### French
+		 * 1er, 2ème, 3ème, …
+		 * 1er avant dernier, 2ème avant dernier, 3ème avant dernier, …
+		 * 0ème, nième
+		 */
 		case "fr":
 			return f((abs, positive) => {
 				const suffix = abs === 1n ? "er" : "ème";
 				const result = `${abs === Infinity ? "ni" : abs}${suffix}`;
 				return positive ? result : `${result} avant dernier`;
 			});
+		/**
+		 * ### Spanish, Portuguese
+		 * Male:
+		 * 1.º, 2.º, 3.º, …
+		 * 1.º al último, 2.º al último, 3.º al último, …
+		 * 0.º, n.º
+		 *
+		 * Female:
+		 * 1.ª, 2.ª, 3.ª, …
+		 * 1.ª a la última, 2.ª a la última, 3.ª a la última, …
+		 * 0.ª, n.ª
+		 */
 		case "es":
+		case "pt":
 			return f((abs, positive) => {
-				const suffix = gender === "female" ? ".ª" : ".º";
+				const suffix = isFemale ? ".ª" : ".º";
 				const result = n`${abs}${suffix}`;
-				return positive ? result : `${result} avant dernier`;
+				const last = isFemale ? "a la última" : "al último";
+				return positive ? result : `${result} ${last}`;
+			});
+		/**
+		 * ### Italian
+		 * Male:
+		 * il 1º, il 2º, il 3º, …, l’8º, il 9º, …, l’11º, il 12º, …, l’80º, l’81º, …, l’800º, …
+		 * l’ultimo, il 2º dall’ultimo, il 3º dall’ultimo, …
+		 * il 0º, l'n-esimo
+		 *
+		 * Female:
+		 * la 1º, la 2º, la 3º, …, l’8º, la 9º, …, l’11º, la 12º, …, l’80º, l’81º, …, l’800º, …
+		 * l’ultima, la 2º dall’ultima, la 3º dall’ultima, …
+		 * la 0º, l’n-esima
+		 */
+		// Formula to calculate the highest digit of a number:
+		// ⌊n⁄(10⌊log₁₀n⌋)⌋
+		// floor(n / 10 / floor(log(n, 10)))
+		case "it":
+			return f((abs, positive) => {
+				const suffixSuperscript = isFemale ? "ª" : "º";
+				const suffix = isFemale ? "a" : "o";
+				// Unicode only marked 8, 11, 80, 800 to use definite article "l’", but not for 8* (81, 888, 8000, ...).
+				const the =
+					isIntPart(abs, 11n) ||
+					abs === Infinity ||
+					abs.toString()[0] === "8" ||
+					!positive && isIntPart(abs, 1n) ?
+						"l’" :
+						isFemale ? "la " : "il ";
+				const result = abs === Infinity ? "n-esim" + suffix : abs + suffixSuperscript;
+				const last = "dall’ultim" + suffix;
+				return the + (positive ? result : abs === 1n ? "ultim" + suffix : `${result} ${last}`);
+			});
+		/**
+		 * ### German
+		 * 1., 2., 3., …
+		 * 1. letzte, 2. letzte, 3. letzte, …
+		 * 0., x-te
+		 */
+		case "de":
+			return f((abs, positive) => {
+				const result = abs === Infinity ? "x-te" : abs + ".";
+				return positive ? result : `${result} letzte`;
+			});
+		/**
+		 * ### Russian
+		 * 1-й, 2-й, 3-й, …
+		 * последний, 2-й до последнего, 3-й до последнего, …
+		 * 0-й, н-й
+		 */
+		case "ru":
+			return f((abs, positive) => {
+				const result = `${abs === Infinity ? "н" : abs}-й`;
+				return positive ? result : abs === 1n ? "последний" : `${result} до последнего`;
 			});
 		default:
 			return f();
